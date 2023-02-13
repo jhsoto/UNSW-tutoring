48111afcad7c5cff35217ff4bcc077f027e810ef
Joshua Sotomayor <joshua.h.sotomayor@gmail.com>
2023-02-13 16:03:21 +1100

    Complete tute code for wk01

--- COMP1521-23T1/wk01/repeat.c
+++ COMP1521-23T1/wk01/repeat.c
@@ -17,9 +17,29 @@ int main() {
     char *str = makeString(to_repeat, n);
     printf("Result: %s\n", str);
 
+    // Remember to free when you malloc!
+    free(str);
+
     return 0;
 }
 
 char *makeString(char to_repeat, int n) {
-    // TODO
+    // We need to malloc as otherwise our string is placed on the stack.
+    // The stack will be cleared when the function ends, deleting our string!
+    char *new_str = malloc(sizeof(*new_str) * (n + 1));
+
+    /* The for loop is equivalent to:
+    int i = 0;
+    while (i < n) {
+        ...
+        i++;
+    }
+    */
+    for (int i = 0; i < n; i++) {
+        new_str[i] = to_repeat;
+    }
+    // Don't forget a null terminator!
+    new_str[n] = '\0';
+
+    return new_str;
 }
