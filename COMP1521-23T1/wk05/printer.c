39e280eaabcda57b050a5552b5f9f417c0c3e05b
Joshua Sotomayor <joshua.h.sotomayor@gmail.com>
2023-03-13 19:57:57 +1100

    Complete tut05 code

--- COMP1521-23T1/wk05/printer.c
+++ COMP1521-23T1/wk05/printer.c
@@ -1,11 +1,17 @@
 // Program to control a printer.
 // The printer has various settings stored as individual bits in a control variable.
 
+// More complex bitwise operations:
+// 
+// clearing - n & ~mask -> sets all masked bits to 0 (clears) without affecting other bits
+// toggling - n ^ mask -> toggles all masked bits i.e. 1s becomes 0s and 0s become 1s
+
 #include <stdio.h>
 #include <stdint.h>
 
 // The bits of this variable control the printer
-uint8_t printerControl = 0; // 0b 0000 0000
+// The printer starts with no ink
+uint8_t printerControl = 0x01; // 0b 0000 0001
 // printerControl = 0 0 0 0 0 0 0 0
 //                          ^ ^ ^ ^
 //                          | | | |
@@ -14,7 +20,7 @@ uint8_t printerControl = 0; // 0b 0000 0000
 //                          | L [SELECT_PRINT]
 //                          L [SELECT_SCAN]
 
-// Whether the printer is out of ink  -> 0 means OK, 1 means out of ink
+// Whether the printer is out of ink -> 0 means OK, 1 means out of ink
 #define NO_INK (0x1)       // 0b 0000 0001
 // Whether to print/scan in colour -> 0 means Black & White, 1 means Colour
 #define COLOUR (0x2)       // 0b 0000 0010
@@ -64,22 +70,49 @@ int main() {
 
 // Print whether the printer is out of ink or not
 void printInkLevel() {
-    // TODO
+    // Test the NO_INK bit using the provided mask
+    if (printerControl & NO_INK) {
+        printf("Out of ink!\n");
+    } else {
+        printf("Ink levels OK.\n");
+    }
 }
 
 // Replace the ink the printer
 void replaceInk() {
-    // TODO
+    // Set the NO_INK bit to 0 to indicate there is ink left
+    printerControl &= ~NO_INK;
+    // Say are in colour print mode with no ink:
+    // printer_control = 0b 0000 0111
+    //         ~NO_INK = 0b 1111 1110 &
+    //                 = 0b 0000 0110
+    //
+    // We clear the NO_INK bit without affecting other bits
 }
 
 // Select the colour and scan modes (assuming no other mode has been set)
 void selectColourScan() {
-    // TODO
+    // Set the colour and select_scan bits (see reverse_bits for a more detailed breakdown)
+    printerControl |= COLOUR;
+    printerControl |= SELECT_SCAN;
 }
 
 // Toggle between printing and scanning mode
 void togglePrintScan() {
-    // TODO
+    // Toggle both the scan and printing mode to swap between the two
+    printerControl ^= SELECT_SCAN;
+    // Say are in colour print mode with no ink:
+    // printer_control = 0b 0000 0111
+    //     SELECT_SCAN = 0b 0000 1000 ^
+    //                 = 0b 0000 1111
+    //
+    // We toggled the SELECT_SCAN bit without affecting other bits
+    printerControl ^= SELECT_PRINT;
+    // printer_control = 0b 0000 0111
+    //    SELECT_PRINT = 0b 0000 0100 ^
+    //                 = 0b 0000 1011
+    //
+    // We toggled the SELECT_PRINT bit without affecting other bits
 }
 
 // Print the current mode the printer is set to
--- COMP1521-23T1/wk05/reverse_bits.c
+++ COMP1521-23T1/wk05/reverse_bits.c
@@ -1,12 +1,51 @@
 // Reverse the bits of a number using bit masks
 
+// SUMMARY:
+//       mask - put a 1 where you want something (setting/extracting) to happen
+//            - you can have a mask that covers multiple bits e.g. mask = 0x3 << 1 = 0b 0000 0110
+//    setting - n | mask -> sets masked bits to 1 without affecting other bits
+// extracting - n & mask -> extracts only masked bits, other bits set to 0
+
 #include <stdio.h>
 #include <stdint.h>
 
 int main() {
     uint16_t n = 0xFACE;
+    // 0xFACE = 0b 1111 1010 1100 1110
     uint16_t n_reverse = 0x0000;
+    // reverse: 0b 0111 0011 0101 1111 = 0x735F
 
-    // TODO: Reverse n's bits
+    for (int i = 0; i < 16; i++) {
+        // Adding a 'u' after a number makes it unsigned - remember always unsigned with bitwise!
+        uint16_t extract_mask = 1u << i;
+        // Say i = 6
+        // 1u = 0b 0000 0000 0000 0001
+        //      left shift 6
+        //   -> 0b 0000 0000 0100 0000
+        //                    ^ bit to extract
+        uint16_t set_mask = 1u << (15 - i);
+        // 1u = 0b 0000 0000 0000 0001
+        //      right shift 15 - 6 = 9
+        //   -> 0b 0000 0010 0000 0000
+        //                ^ bit to set
+
+        // n & extract_mask:
+        //   0b 1111 1010 1100 1110
+        // & 0b 0000 0000 0100 0000
+        // = 0b 0000 0000 0100 0000
+        //
+        // The extracted bit was set to 1, so we should set the corresponding bit in n_reverse
+        // If it was 0, there would be no need to set anything, n_reverse has all zeroes initially
+        if (n & extract_mask) {
+            // n_reverse | set_mask:
+            //   0b 0111 0000 0000 0000   <- let's assume n_reverse has been set correctly so far
+            // | 0b 0000 0010 0000 0000
+            // = 0b 0111 0010 0000 0000
+            //
+            // We have set the bit to 1
+            n_reverse |= set_mask;
+        }
+    }
+
     printf("%X\n", n_reverse);
 }
