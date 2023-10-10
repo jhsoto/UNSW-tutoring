a060d3277e6a75d66cf69cbab45cac42e78dddbb
Joshua Sotomayor <joshua.h.sotomayor@gmail.com>
2023-10-10 12:27:18 +1100

    Finished wk05 tute code

--- COMP1521-23T3/wk05/bitwise.c
+++ COMP1521-23T3/wk05/bitwise.c
@@ -6,10 +6,13 @@
 
 int main() {
     // Always use UNSIGNED integers when working with bitwise operations
-    // Signed integers have strange behaviour when being right shifted (more on why next time!)
-    uint16_t a = 0x5555; // = 0b 0101 0101 0101 0101
-    uint16_t b = 0xAA55; // = 0b 1010 1010 1010 1010
-    uint16_t c = 0x0101; // = 0b 0000 0001 0000 0001
+    // Signed integers have strange behaviour when being right shifted (more on why next week!)
+    uint16_t a = 0x5555;
+    // 0b 0101 0101 0101 0101
+    uint16_t b = 0xAA55;
+    // 0b 1010 1010 0101 0101
+    uint16_t c = 0x0001;
+    // 0b 0000 0000 0000 0001
 
     // Quick note on the printf format used here:
     // %04x
@@ -19,25 +22,42 @@ int main() {
 
     // OR
     printf("a | b  = 0x%04x\n", a | b);
+    // a - 0b 0101 0101 0101 0101
+    // b - 0b 1010 1010 0101 0101  OR     - 1 if EITHER bit is 1, else 0
+    //  -> 0b 1111 1111 0101 0101
+    //     0x F    F    5    5
+    //  -> 0x FF55
 
     // AND
-    // printf("a & b  = 0x%04x\n", a & b);
+    printf("a & b  = 0x%04x\n", a & b);
+    // a - 0b 0101 0101 0101 0101
+    // b - 0b 1010 1010 0101 0101  AND    - 1 if BOTH bits are 1, else 0
+    //  -> 0b 0000 0000 0101 0101
+    //     0x 0055
 
-    // XOR
-    // printf("a ^ b  = 0x%04x\n", a ^ b);
+    // XOR - exclusive or
+    printf("a ^ b  = 0x%04x\n", a ^ b);
+    // a - 0b 0101 0101 0101 0101
+    // b - 0b 1010 1010 0101 0101  XOR    - 1 if exactly one of the bits are 1 (EITHER but NOT BOTH), else 0
+    //  -> 0b 1111 1111 0000 0000
+    //     0x FF00
 
     // AND + NOT
-    // printf("a & ~b = 0x%04x\n", a & ~b);
+    printf("a & ~b = 0x%04x\n", a & ~b);
+    //  a - 0b 0101 0101 0101 0101
+    // ~b - 0b 0101 0101 1010 1010  AND   - NOT swaps 0 to 1, and 1 to 0
+    //   -> 0b 0101 0101 0000 0000
+    //      0x 5500
 
     // Left shift
-    // printf("c << 6 = 0x%04x\n", c << 6);
+    printf("c << 6 = %d\n", c << 6);
+    // c - 0b 0000 0000 0000 0001 << 6    - Shift all bits to the left, fills with 0s
+    //  -> 0b 0000 0000 0100 0000
+    //     0x 0040
 
     // Right shift
-    // printf("a >> 4 = 0x%04x\n", a >> 4);
-    /*
-    Which of the following is the correct value of a >> 4?
-    A) 0b 0101 0101 0101 0101 = 0xAAAA
-    B) 0b 0000 0101 0101 0101 = 0x0AAA
-    C) 0b 1111 0101 0101 0101 = 0xFAAA
-    */
+    printf("a >> 4 = 0x%04x\n", a >> 4);
+    // a - 0b 0101 0101 0101 0101 >> 4        - Shift all bits to the right, fills with 0s (usually)
+    //  -> 0b 0000 0101 0101 0101
+    //     0x 0555
 }
